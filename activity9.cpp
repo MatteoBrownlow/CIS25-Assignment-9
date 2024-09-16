8a1430cad69720de9ac03343db969fd65ab61977
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-16 10:01:48 -0700

    Update activity9.cpp

---
+++ activity9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+  string answer;
+  cout << "Are you a US citizen? Type yes or no" << endl;
+  cin >> answer;
+  if(answer == yes){
+  }
+  cout << "You are not eligible for a job" << endl;
+  
+  return 0;
+}
