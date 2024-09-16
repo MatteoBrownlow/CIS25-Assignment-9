46e22147a6597e1f61cb2b31c89746497c9596fd
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-16 10:26:09 -0700

    Update activity9.cpp

--- activity9.cpp
+++ activity9.cpp
@@ -2,25 +2,26 @@
 using namespace std;
 
 int main(){ 
-  string answer;
-  cout << "Are you a US citizen? Type yes or no" << endl;
-  cin >> answer;
-  if(answer == yes){
-    cout << "Do you have a bachelor's degree? Type yes or no" << endl;
-    cin >> answer;
-    if(answer == yes){
-      break;
-    }
-    if(answer == no){
-      cout << "Do you have at least two years experience? Type yes or no" << endl;
-      cin << answer;
-      if(answer == no){
+    string UScitizen, twoYearsExperience, bachelors;
+    cout << "Are you a US citizen? Type yes or no" << endl;
+    cin >> UScitizen;
+    if(UScitizen == "no"){
         cout << "You are not eligible for a job" << endl;
         return 0;
-      }
     }
-    
-  }
-  cout << "You are eligible for a job" << endl;
-  return 0;
+    cout << "Do you have a bachelor's degree? Type yes or no" << endl;
+    cin >> bachelors;
+    if(bachelors == "yes"){
+        cout << "You are eligible for a job" << endl;
+        return 0;
+    }
+    cout << "Do you have at least two years experience? Type yes or no" << endl;
+    cin >> twoYearsExperience;
+    if(twoYearsExperience == "yes"){
+        cout << "You are eligible for a job" << endl;
+        return 0;
+    }
+    cout << "You are not eligible for a job" << endl;
+    return 0;
+  
 }
