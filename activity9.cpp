e4e279c2acab0a21cfbcb48a0a9d8f643b2533c4
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-16 10:05:54 -0700

    Update activity9.cpp

--- activity9.cpp
+++ activity9.cpp
@@ -6,8 +6,17 @@ int main(){
   cout << "Are you a US citizen? Type yes or no" << endl;
   cin >> answer;
   if(answer == yes){
+    cout << "Do you have a bachelor's degree? Type yes or no" << endl;
+    cin >> answer;
+    if(answer == no){
+      cout << "Do you have at least two years experience? Type yes or no" << endl;
+      cin << answer;
+      if(answer == no){
+        cout << "You are not eligible for a job" << endl;
+        return 0;
+      }
+    }
   }
-  cout << "You are not eligible for a job" << endl;
-  
+  cout << "You are eligible for a job" << endl;
   return 0;
 }
